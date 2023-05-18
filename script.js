e792f84eda37c238144cea97b6472cd848c658aa
sabujmahonto <sabujmahonto@gmail.com>
2023-05-18 20:47:09 +0600

    Bit O(n) -linear tmie complexity done

---
+++ script.js
@@ -0,0 +1,21 @@
+// algorithm
+// time complexity
+// space complexity
+// asymptotic notations 
+// 1. Big o notations
+// 2. Omega notations 
+// 3. Theta notations
+
+// Find the time complexity 
+
+const arr = [10,20,30,40,50];
+function sum(arr){
+   let sum = 0;                           // executing time  =  1
+
+   for(let i = 0; i < arr.length; i++){
+    sum = sum + arr.at(i)                //  executing time  =  arr.length = 5  / n + 2 / or n
+   }
+   return sum;                          //  executing time  =   1 
+}                                                                                                  
+
+console.log(sum(arr)); // O(n) -linear time  complexity
